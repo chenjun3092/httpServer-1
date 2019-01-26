ee19c24f42c1df499e08fee0a3dfc5e8f6842755
fushenshen <hellofss@foxmail.com>
2019-01-26 23:08:49 +0800

    进程奔溃重启

--- source/main.c
+++ source/main.c
@@ -1,4 +1,11 @@
 #include <stdlib.h>
+#include <init_config.h>
+#include <log.h>
+#include "server.h"
+
+const char *jpath;
+extern server_config_package *p;
+#include <stdlib.h>
 #include <zconf.h>
 #include <init_config.h>
 #include <log.h>
@@ -7,15 +14,42 @@
 const char *jpath;
 extern server_config_package *p;
 
-#if 0
+void do_restart (int signo){
+    pid_t pid;
+    pid = fork();
+    if(!pid){
+        server_init(jpath);
+        write_log(INFO_L, getpid(), __FUNCTION__, __LINE__, "服务器重启");
+    }else {
+        /*parent process*/
+        signal(SIGCHLD, do_restart);
+        wait(NULL);
+    }
+}
+
+#if 1
+
 int main (int argc, char *argv[]) {
     if (argc != 2) {
         fprintf(stderr, "ex: httpserv json_path");
         exit(1);
     }
     jpath = argv[1];
-    server_init(jpath);
-
+    pid_t pid;
+    if ((pid = fork()) < 0) {
+        fprintf(stderr, "http服务器初始化失败");
+        fflush(stdout);
+    } else if (pid > 0) {
+        fprintf(stdout,"watch process pid:%d\n",getpid());
+        fflush(stdout);
+        signal(SIGCHLD, do_restart);
+        wait(NULL);
+    } else {
+        fprintf(stdout,"worker process pid:%d\n",getpid());
+        fflush(stdout);
+        server_init(jpath);
+    }
     return 0;
 }
+
 #endif
