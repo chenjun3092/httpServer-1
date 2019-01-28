d85a87e2264cad92b1ee915866984dc297e0da3f
fushenshen <hellofss@foxmail.com>
2019-01-28 14:46:54 +0800

    fix bug

--- source/server/server.c
+++ source/server/server.c
@@ -449,6 +449,8 @@ void event_init_listener (struct evconnlistener *listener, struct event_base *ba
                                        36, (struct sockaddr *) &serv, sizeof(serv));
     if (listener != NULL) {
         write_log(INFO_L, getpid(), __FUNCTION__, __LINE__, "绑定服务器端口成功");
+        fprintf(stdout,"服务器启动成功\n");
+        fflush(stdout);
     } else {
         /**
          * 启动配置文件中选定的备用端口
@@ -460,8 +462,6 @@ void event_init_listener (struct evconnlistener *listener, struct event_base *ba
         if (listener != NULL) {
             write_log(INFO_L, getpid(), __FUNCTION__, __LINE__, "备用服务器端口启动成功");
         } else {
-            fprintf(stderr, "备用服务器端口启动失败\n");
-            fflush(stderr);
             write_log(EMERGE_L, getpid(), __FUNCTION__, __LINE__, "备用服务器端口启动失败");
             exit(1);
         }
